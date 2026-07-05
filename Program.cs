259211e363b6b565d38fdca80fee0ae48b8cf894
black <black@example.com>
2026-07-05 18:24:20 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello();
+        }
+
+        static void Hello()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
